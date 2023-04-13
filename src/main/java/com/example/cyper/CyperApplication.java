77a4f1fcec6301c24a84b3c9c6e42b51189ba8ed
Ivan Tavarez <ivantavarez2@gmail.com>
2023-04-13 12:28:41 -0400

    Spring boot 3 initial setup

---
+++ src/main/java/com/example/cyper/CyperApplication.java
@@ -0,0 +1,13 @@
+package com.example.cyper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cyper/CyperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cyper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
